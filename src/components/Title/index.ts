a10670e1ba2f73374e675c3facdfa2bbac6b2fda
LuisMarchio03 <luisgabrielmarchio75@gmail.com>
2022-11-13 09:32:32 -0300

    style: re-style title component

--- src/components/Title/index.ts
+++ src/components/Title/index.ts
@@ -2,14 +2,12 @@ import { styled } from '../../styles'
 
 export const Title = styled('h1', {
   position: 'relative',
-  fontWeight: 700,
+  // fontWeight: 700,
   letterSpacing: '0.05rem',
   marginBottom: '25px',
   paddingBottom: '25px',
 
   borderBottom: '1px solid rgba(255,255,255,0.2)',
-
-  fontSize: '2.5rem',
 
   '&::after': {
     content: '""',
