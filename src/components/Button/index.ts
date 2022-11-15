b2e54b441f661ba3d973cb3e4ebe9bf158c87aaa
LuisMarchio03 <luisgabrielmarchio75@gmail.com>
2022-11-15 11:09:11 -0300

    refactor: improving component button styles

--- src/components/Button/index.ts
+++ src/components/Button/index.ts
@@ -7,17 +7,55 @@ export const Button = styled('button', {
   padding: '12px 36px',
   cursor: 'pointer',
 
+  transition: '0.2s',
+
   variants: {
     variant: {
       primary: {
         backgroundColor: '$yellow500',
         border: '2px solid $yellow500',
         color: '$gray600',
+
+        '&:hover': {
+          backgroundColor: '$yellow700',
+        },
+
+        '&:disabled': {
+          backgroundColor: '$yellow700',
+          cursor: 'not-allowed',
+        },
       },
       secondary: {
         backgroundColor: 'transparent',
         border: '2px solid $yellow500',
         color: '$yellow500',
+
+        '&:hover': {
+          color: '$yellow700',
+          border: '2px solid $yellow700',
+        },
+
+        '&:disabled': {
+          color: '$yellow700',
+          border: '2px solid $yellow700',
+          cursor: 'not-allowed',
+        },
+      },
+      danger: {
+        backgroundColor: 'transparent',
+        border: '2px solid $red500',
+        color: '$red500',
+
+        '&:hover': {
+          color: '$red700',
+          border: '2px solid $red700',
+        },
+
+        '&:disabled': {
+          color: '$red700',
+          border: '2px solid $red700',
+          cursor: 'not-allowed',
+        },
       },
     },
   },
--- src/components/Button/stories/Button.stories.tsx
+++ src/components/Button/stories/Button.stories.tsx
@@ -4,25 +4,52 @@ import { ComponentStory, ComponentMeta } from '@storybook/react'
 import { Button } from '..'
 
 import { globalStyles } from '../../../styles/global'
+import { styled } from '../../../styles'
 globalStyles()
+
+const Container = styled('div', {
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '10px',
+})
 
 export default {
   title: 'Components/Button',
   component: Button,
 } as ComponentMeta<typeof Button>
 
-const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />
+export const UsagePrimary: ComponentStory<typeof Button> = (args) => (
+  <Container>
+    <Button type="button" children="Primary" variant="primary" />
+    <Button
+      type="button"
+      children="Primary - Disabled"
+      disabled
+      variant="primary"
+    />
+  </Container>
+)
 
-export const UsagePrimary = Template.bind({})
-UsagePrimary.args = {
-  type: 'button',
-  children: <>Click me</>,
-  variant: 'primary',
-}
+export const UsageSecondary: ComponentStory<typeof Button> = () => (
+  <Container>
+    <Button type="button" children="Secondary" variant="secondary" />
+    <Button
+      type="button"
+      children="Secondary - Disabled"
+      disabled
+      variant="secondary"
+    />
+  </Container>
+)
 
-export const UsageSecondary = Template.bind({})
-UsageSecondary.args = {
-  type: 'button',
-  children: <>Click me</>,
-  variant: 'secondary',
-}
+export const UsageDanger: ComponentStory<typeof Button> = () => (
+  <Container>
+    <Button type="button" children="Danger" variant="danger" />
+    <Button
+      type="button"
+      children="Danger - Disabled"
+      disabled
+      variant="danger"
+    />
+  </Container>
+)
