1d080967a635477a65a7dec680e2d3d3a2dcc8b7
LuisMarchio03 <luisgabrielmarchio75@gmail.com>
2022-11-15 14:19:02 -0300

    style: docs hero component

---
+++ src/App.ts
@@ -0,0 +1,9 @@
+import { styled } from './styles'
+
+export const Container = styled('main', {
+  width: '100%',
+
+  button: {
+    marginTop: '1.2rem',
+  },
+})
--- src/components/Hero/stories/Hero.stories.tsx
+++ src/components/Hero/stories/Hero.stories.tsx
@@ -4,6 +4,8 @@ import { ComponentStory, ComponentMeta } from '@storybook/react'
 
 import { Hero } from '..'
 import { Title } from '../../Title'
+import { Button } from '../../Button'
+import { Container } from '../../../App'
 
 import { globalStyles } from '../../../styles/global'
 globalStyles()
@@ -36,15 +38,16 @@ UsageWithImage.args = {
   image:
     'url(https://viagemeturismo.abril.com.br/wp-content/uploads/2021/04/road-3114475__340.jpg), rgba(0, 0, 0, 0.5)',
   children: (
-    <>
+    <Container>
       <Title>
         Ganhe sua <strong>liberdade</strong> <br /> para ir e vir
       </Title>
       <ul>
-        <li>One</li>
-        <li>Two</li>
-        <li>Three</li>
+        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
       </ul>
-    </>
+      <Button variant="secondary">Matricule-se agora</Button>
+    </Container>
   ),
 }
