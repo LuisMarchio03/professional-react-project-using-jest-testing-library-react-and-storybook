09d961091edb53acac5bbf9d9d95483dda970625
LuisMarchio03 <luisgabrielmarchio75@gmail.com>
2022-11-14 21:46:05 -0300

    feat: create button component varient primary

---
+++ src/components/Button/index.ts
@@ -0,0 +1,13 @@
+import { styled } from '../../styles'
+
+export const Button = styled('button', {
+  fontSize: '$md',
+  fontWeight: 600,
+  textTransform: 'uppercase',
+  padding: '12px 36px',
+  cursor: 'pointer',
+
+  backgroundColor: '$yellow500',
+  border: '2px solid $yellow500',
+  color: '$gray600',
+})
---
+++ src/components/Button/stories/Button.stories.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+
+import { Button } from '..'
+
+import { globalStyles } from '../../../styles/global'
+globalStyles()
+
+export default {
+  title: 'Components/Button',
+  component: Button,
+} as ComponentMeta<typeof Button>
+
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />
+
+export const UsagePrimary = Template.bind({})
+UsagePrimary.args = {
+  type: 'button',
+  children: <>Click me</>,
+}
+
+export const UsageSecondary = Template.bind({})
+UsageSecondary.args = {
+  type: 'button',
+  children: <>Click me</>,
+}
