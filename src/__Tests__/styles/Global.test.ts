330713b2558206d643613b2b5aebe837baf47220
LuisMarchio03 <luisgabrielmarchio75@gmail.com>
2022-11-14 21:11:54 -0300

    test: finished test Global Syles

--- src/__Tests__/styles/Global.test.ts
+++ src/__Tests__/styles/Global.test.ts
@@ -1,7 +1,8 @@
 import { globalStyles } from '../../styles/global'
+import { styled } from '../../styles'
 
 describe('Global Styles', () => {
-  it('should render global styles', () => {
+  test('should render global styles', () => {
     expect(globalStyles).toMatchSnapshot()
   })
 })
